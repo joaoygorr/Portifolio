8375ace9a50937fe878f626c66637ec96232c744
joaoygorr <ygorvieira2002@gmail.com>
2023-06-28 16:11:33 -0400

    fix: menu

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,10 @@
 import "../styles/style.scss";
 import "../styles/reset.scss";
-import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css"
+import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+
+const circle = document.querySelector(".box-circle-menu");
+const boxNavigationList = document.querySelector(".box-navigation-list");
+
+circle?.addEventListener("click", () => {
+    boxNavigationList?.classList.toggle("active");
+});
