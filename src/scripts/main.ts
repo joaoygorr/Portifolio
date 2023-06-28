f306498caf80601a39a3d9f4cbef754512ce1c57
joaoygorr <ygorvieira2002@gmail.com>
2023-06-28 18:28:20 -0400

    update portifolio

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,5 +4,9 @@ import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 const circle: HTMLDivElement = document.querySelector(".box-circle-menu")!;
 const boxNavigationList: HTMLUListElement = document.querySelector(".box-navigation-list")!;
+const iconHouse: HTMLElement = document.querySelector(".bi-list")!;
 
-circle!.onclick = () => boxNavigationList?.classList.toggle("active");
+circle!.onclick = () => {
+    boxNavigationList?.classList.toggle("active");
+    iconHouse.classList.toggle("bi-x");
+};
