1c6c4be9d895f0dd2fd50a1e1555cb8d283e2dd8
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-16 15:02:04 -0400

    fix: ajuste links css

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,3 @@
-import "../styles/style.scss";
-import "../styles/reset.scss";
-import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
-
 const circle: HTMLDivElement = document.querySelector(".box-circle-menu")!;
 const boxNavigationList: HTMLUListElement = document.querySelector(".box-navigation-list")!;
 const iconHouse: HTMLElement = document.querySelector(".bi-list")!;
